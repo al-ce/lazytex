44b567a1da11d2648ee225a3ec7b3c89a79bf0e7
Alessandro Ceserani <aceserani@gmail.com>
2023-04-11 16:58:23 -0500

    fix: Get args from main() instead of passing them
    I'm passings args as a default argument to main for testing purposes, which I'm now realizing may have been a bad design choice. This changne lets me keep my test structure in place. This project is basically a unit-test exercise anyway, so good practice

--- lazytex/__main__.py
+++ lazytex/__main__.py
@@ -3,44 +3,6 @@ import pyperclip
 import lazytex.text_utils as tu
 
 
-def main(args):
-
-    if not (args.input_file or args.statement):
-        print("Either an input file or a statement must be provided, but not both.")
-        return 1
-
-    if args.statement:
-        output_latex = tu.convert_to_latex(args.statement)
-        output_type = "Statement"
-        print("Converted statement to LaTeX:\n")
-        print(output_latex)
-        print()
-
-    elif args.input_file:
-        lines = tu.get_file_lines(args.input_file)
-        output_latex = "".join(tu.generate_table(lines))
-        output_type = "Table"
-        print(f"\nConverted {args.input_file} to LaTeX table.\n")
-
-    if args.write_to:
-        write_to = args.write_to
-        tu.write_table_to_file(output_latex, write_to)
-        print(f"{output_type} written to {write_to}")
-        return 0
-    elif args.append_to:
-        write_to = args.append_to
-        tu.append_latex_to_existing_file(output_latex, write_to)
-        print(f"{output_type} appended to {write_to}")
-        return 0
-
-    pyperclip.copy(output_latex)
-    if pyperclip.paste() == output_latex:
-        print(f"{output_type} copied to clipboard.")
-    else:
-        print("Failed to copy statement to clipboard.")
-    return 0
-
-
 def parse_args(args=None):
     parser = argparse.ArgumentParser(
         description='Converts logical statements in a simple format to LaTeX. If an output file is specified with the -w flag, will write the converted LaTeX statements to the output file, or with the -a flag, append to it. Otherwise, they will be copied to the user\'s clipboard')
@@ -80,7 +42,47 @@ def parse_args(args=None):
     return args
 
 
+def main(args=None):
+
+    # args is passed to main for testing purposes
+    if not args:
+        args = parse_args()
+
+    if not (args.input_file or args.statement):
+        print("Either an input file or a statement must be provided, but not both.")
+        return 1
+
+    if args.statement:
+        output_latex = tu.convert_to_latex(args.statement)
+        output_type = "Statement"
+        print("Converted statement to LaTeX:\n")
+        print(output_latex)
+        print()
+
+    elif args.input_file:
+        lines = tu.get_file_lines(args.input_file)
+        output_latex = "".join(tu.generate_table(lines))
+        output_type = "Table"
+        print(f"\nConverted {args.input_file} to LaTeX table.\n")
+
+    if args.write_to:
+        write_to = args.write_to
+        tu.write_table_to_file(output_latex, write_to)
+        print(f"{output_type} written to {write_to}")
+        return 0
+    elif args.append_to:
+        write_to = args.append_to
+        tu.append_latex_to_existing_file(output_latex, write_to)
+        print(f"{output_type} appended to {write_to}")
+        return 0
+
+    pyperclip.copy(output_latex)
+    if pyperclip.paste() == output_latex:
+        print(f"{output_type} copied to clipboard.")
+    else:
+        print("Failed to copy statement to clipboard.")
+    return 0
+
+
 if __name__ == '__main__':
-    import sys
-    args = parse_args(sys.argv[1:])
-    main(args)
+    main()
